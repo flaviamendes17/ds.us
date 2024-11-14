3742e23e390f2cf907af4ff48496e017a0dc6a53
Flavia Mendes <flavinharamendes@gmail.com>
2024-11-14 11:29:27 -0300

    feat: criação das classes

--- src/models/jogadores/jogo.js
+++ src/models/jogadores/jogo.js
@@ -1,1 +1,28 @@
+class Jogo {
+    constructor(nome, quantidadeJogadores, mapa) {
+        this.nome = nome;
+        this.quantidadeJogadores = quantidadeJogadores;
+        this.mapa = mapa;
+    } 
+}
+class Desenvolvedor {
+    constructor(id, nome, caracteristicas, culpado) {
+        this.id = id;
+        this.nome = nome;
+        this.caracteristicas = caracteristicas;
+        this.mapa = mapa;
+        this.culpado = culpado;
+    } 
+}
+class Sabotador {
+    constructor(id, nome, caracteristicas, culpado) {
+        this.id = id;
+        this.nome = nome;
+        this.caracteristicas = caracteristicas;
+        this.mapa = mapa;
+        this.culpado = culpado;
+    } 
+}
 
+
+module.exports = Jogo;
